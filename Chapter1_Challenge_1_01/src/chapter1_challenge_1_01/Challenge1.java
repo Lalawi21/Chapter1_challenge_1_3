d64b0fdf7bf813fbe5cdd05b5af69614d83629fc
Lalawi21 <lealawitteferi@gmail.com>
2025-10-16 15:32:59 +0300

    Chapter1_Challenge_1_01

---
+++ Chapter1_Challenge_1_01/src/chapter1_challenge_1_01/Challenge1.java
@@ -0,0 +1,42 @@
+package chapter1_challenge_1_01;
+
+/**
+ *
+ * @author lalawit
+ */
+public class Challenge1 {
+    // Function to decode the cryptic message
+    public static void decodeMessage(int input) {
+        // Input: positive integer
+        int num = input;
+
+        // Extract the first digit
+        int firstDigit = (int) Math.floor(num / Math.pow(10, Math.floor(Math.log10(num))));
+
+        // Extract the last digit
+        int lastDigit = num % 10;
+
+        // Calculate the product of the first and last digit
+        int product = firstDigit * lastDigit;
+
+        // Extract the second digit
+        int secondDigit = (int) Math.floor((num % 100) / 10);
+
+        // Extract the second-last digit
+        int secondLastDigit = (int) Math.floor((num / 10) % 10);
+
+        // Calculate the sum of the second digit and the second-last digit
+        int sum = secondDigit + secondLastDigit;
+
+        // Create the final code by concatenating product and sum
+        String finalCode = String.valueOf(product) + sum;
+
+        // Output the decrypted code
+        System.out.println("The decrypted code is: " + finalCode);
+    }
+
+    public static void main(String[] args) {
+        // Example input
+        decodeMessage(13579);
+    }
+}
